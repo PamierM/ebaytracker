6a390ac10fda1af1d7bea3901d23be1d51830354
Pamier <46360088+PamierM@users.noreply.github.com>
2020-06-02 22:08:22 -0700

    Update WebScraping.py

--- WebScraping.py
+++ WebScraping.py
@@ -5,32 +5,33 @@ import requests
 import smtplib
 import time
 
-#Insert the URL of the desired eBay product here
-url = "https://www.ebay.com/itm/Canon-PIXMA-MG3620-Home-Office-Wireless-All-In-One-Inkjet-Printer-INK-INCLUDED/313100926099?epid=232027656&hash=item48e644fc93:g:Gt0AAOSwASZevsMc"
+url = input("Enter a space then your desired eBay listing link: ")
+desired_price_string = input("Enter your desired price:")
+desired_price = float(desired_price_string)
 #Insert your header here (look up your header to find it), optional if not wanted deleted line of code
 headers = {"User-Agent": 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0'}
-#insert your gmail here that you want the server to send/receive from
-email = 'mail@gmail.com'
+email = input("Enter your Gmail: ")
+password = input("Enter your Gmail password: ")
 
 def check_price():
     page = requests.get(url, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
     title = soup.find(id="itemTitle").get_text()
-    price = soup.find(id="prcIsum").get_text()
-    converted_price = float(price[4:10])
+    price = soup.find(id="prcIsum").get_text().replace('/ea', '')
+    converted_price = float(price[4:20])
 
-    if(converted_price > 50):
+    if(converted_price <= desired_price):
         send_mail()
 
 def send_mail():
     subject = "Price Drop"
-    mailtext = "Subjects:"+subject+'\n\n'+'Your item is at your deisred price: ' + url
+    mailtext = "Subjects:"+subject+'\n\n'+'Your item is at your desired price: ' + url
 
     server = smtplib.SMTP(host = 'smtp.gmail.com', port=587)
     server.ehlo()
     server.starttls()
     #Type your password in the password string
-    server.login(email,'password')
+    server.login(email,password)
     server.sendmail(email, email, mailtext)
     pass
 
